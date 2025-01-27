a3bc1c79e1f7c37ebcd03acde958d4613e32d28c
GvF1000 <gleb.vf123@gmail.com>
2025-01-27 12:19:59 -0800

    create card struct

--- bj.cpp
+++ bj.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+
+struct Card
+{
+    char rank;
+    std::string suite;
+    bool faceCard;
+
+    Card(char r, std::string s) : rank(r), suite(s) 
+    {
+        faceCard = (rank == 'K' || rank == 'Q' || rank == 'J');
+    }
+
+    void displayCard()
+    {
+        std::cout << "Rank: " << rank << "\n";
+        std::cout << "Suite: " << suite << "\n";
+    }
+
+};
+
 
 int main()
 {
-    std::cout << "test";
+    Card random = Card('K', "Club");
+    random.displayCard();
     return 0;
 }
