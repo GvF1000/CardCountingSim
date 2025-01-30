4a3456d9fd8e863572b43cdbe15a10d2b7865f09
GvF1000 <gleb.vf123@gmail.com>
2025-01-30 12:05:25 -0800

    adjust hand logic to also use unique pointers and fix referencing

--- Hand.cpp
+++ Hand.cpp
@@ -1,8 +1,8 @@
 #include "Hand.h"
 
-Hand::Hand(std::vector<Card*> const handCards, const int betAmount) : cards(handCards)
+Hand::Hand(std::vector<std::unique_ptr<Card>> const handCards, const int betAmount) : cards(handCards)
 {
-    for (Card* card : cards) 
+    for (std::unique_ptr<Card>& card : cards) 
     {
         score += card->score;
     }
@@ -10,7 +10,7 @@ Hand::Hand(std::vector<Card*> const handCards, const int betAmount) : cards(hand
 
 void Hand::displayHand() 
 {
-    for (Card* card : cards) 
+    for (std::unique_ptr<Card>& card : cards) 
     {
         card->displayCard();
     }
--- Hand.h
+++ Hand.h
@@ -2,15 +2,16 @@
 #define HAND_H
 
 #include <vector>
+#include <memory>
 #include "Card.h"
 
 struct Hand
 {
     int score = 0;
     int betAmount = 0;
-    std::vector<Card*> cards;
+    std::vector<std::unique_ptr<Card>> cards;
 
-    Hand(std::vector<Card*> const handCards = {}, const int betAmount = 0);
+    Hand(std::vector<std::unique_ptr<Card>> const handCards = {}, const int betAmount = 0);
     void displayHand();
 };
 
