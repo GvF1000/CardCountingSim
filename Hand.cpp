2e1766358716dce23d77f7d4c79ba2b7f05bcffa
GvF1000 <gleb.vf123@gmail.com>
2025-01-31 12:47:33 -0800

    add addCard function to handle adding cards into the Hand struct and automatically updating the score

--- Hand.cpp
+++ Hand.cpp
@@ -8,6 +8,12 @@ Hand::Hand(std::vector<std::unique_ptr<Card>>& const handCards, const int betAmo
     }
 }
 
+void Hand::addCard(std::unique_ptr<Card>& card)
+{
+    cards.push_back(std::move(card));
+    score += cards.back()->score;
+}
+
 void Hand::displayHand() 
 {
     for (std::unique_ptr<Card>& card : cards) 
