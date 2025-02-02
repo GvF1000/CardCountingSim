52ba674337d871d0bc50124a18c857eaa0cdc4c5
GvF1000 <gleb.vf123@gmail.com>
2025-02-01 18:44:30 -0800

    add calculate score method

--- Hand.h
+++ Hand.h
@@ -7,12 +7,18 @@
 
 struct Hand
 {
+private:
     int score = 0;
     int betAmount = 0;
     std::vector<std::unique_ptr<Card>> cards;
 
+    int calculateScore();
+
+public:
     Hand(std::vector<std::unique_ptr<Card>>& const handCards = {}, const int betAmount = 0);
+    const std::vector<std::unique_ptr<Card>>& getCards();
     int getScore();
+    int getBetAmount();
     void addCard(std::unique_ptr<Card>& card);
     void displayHand();
 };
