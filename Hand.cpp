b772bd3be4658bcd91ab8e126ebf606beaaef9ab
GvF1000 <gleb.vf123@gmail.com>
2025-02-01 18:44:08 -0800

    add calculate score method

--- Hand.cpp
+++ Hand.cpp
@@ -2,16 +2,27 @@
 
 Hand::Hand(std::vector<std::unique_ptr<Card>>& const handCards, const int betAmount) : cards(handCards)
 {
-    for (std::unique_ptr<Card>& card : cards) 
+    calculateScore();
+}
+
+const std::vector<std::unique_ptr<Card>>& Hand::getCards()
+{
+    return cards;
+}
+
+int Hand::calculateScore()
+{
+    score = 0;
+    for (const std::unique_ptr<Card>& card : getCards()) 
     {
-        score += card->score;
+        score += card->getScore();
     }
 }
 
 void Hand::addCard(std::unique_ptr<Card>& card)
 {
     cards.push_back(std::move(card));
-    score += cards.back()->score;
+    score += cards.back()->getScore();
 }
 
 int Hand::getScore()
